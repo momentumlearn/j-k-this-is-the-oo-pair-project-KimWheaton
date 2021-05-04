08028832c6b7e5b6a9ce36da34b1cb27f2ef3aca
KimWheaton <kwheatonart@gmail.com>
2021-05-04 18:05:15 -0400

    debugging work in class

--- wheel_game.py
+++ wheel_game.py
@@ -3,13 +3,12 @@ import random
 # ask user for mode "Easy", "Normal", "Hard"
 
 class Word:
-    def __init__(self, word_length):
-        word = open('/Users/kimwheaton/Momentum/homework/j-k-this-is-the-oo-pair-project-KimWheaton/words.txt', 'r')
-        word = word.read()
-        self.word = word
-        self.word_length = word_length 
+    def __init__(self):
+        word_list = open(words.txt)
+        word_list = word_list.read()
+        self.word = random.choice(word_list.read)
+        # self.word_length = word_length 
         # use word_length to build a function for difficulty: "easy", "normal" or "hard" levels
-        word = random.choice(word.read())
         # return word.upper()
  
     def show_mystery_word(self):
@@ -25,31 +24,31 @@ class Game:
     def attempt_guess(self):
         total_guesses= 8
         guesses = 0
-        word = "spam"
+        word = Word()
         gameboard = show_mystery_word(word)
         print(gameboard)
         gameboard = list(gameboard)
         while guesses < total_guesses:
             guess = input("Please guess a letter: ")
-            if guess.upper() not in word:
+            if guess.lower() not in word:
                 guesses += 1
                 print("Incorrect, guesses remaining:", total_guesses - guesses, "Try again.")
             else: 
                 print("Correct!")
            
             for letter in word:
-                if guess.upper() == letter:
-                    gameboard[word.index(letter) * 2] = guess.upper()
+                if guess.lower() == letter:
+                    gameboard[word.index(letter) * 2] = guess.lower()
                   
             print("".join(gameboard))
             print(guesses)
             if not "_" in gameboard:
                 print("Congratulations, You win! Would you like to play again?")
 
-def show_mystery_word(word):
+def show_mystery_word(Word.word):
     return '_ ' * len(word)
 
-difficulty = input("Do you want to play difficulty level Easy, Normal, or Hard?" )
+difficulty = input("Do you want to play difficulty level Easy, Normal, or Hard? ")
 new_game = Game(difficulty)
 new_game.attempt_guess()
 
